c046091cf53ba5d5a81d36d9c4b73f95e4da3fe1
Pavel <pawelztef@gmail.com>
2019-06-28 19:04:07 +0100

    MANAGE USERS THROUGH A CRUD INTERFACE / DEVISE

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
 
-  scope '/admin' do
+  # scope '/admin' do
     # EXAMPLE CONFIGURATIONS DEVISE PATHS
     # devise_for :admins, :controllers => { :registrations => 'registrations' },
     #   :path => 'admins',
@@ -10,15 +10,16 @@ Rails.application.routes.draw do
     #                    :sign_out => 'logout',
     #                    :password => 'secret',
     #                    :confirmation => 'verification' }
+  # end
+  #
     devise_for :users, 
-      :controlers => {:registrations => 'registrations'},
-      :path => 'users',
-      :path_names => { :sign_in => 'login' }
-  end
+               :path_names => { :sign_in => 'login',
+                                :sign_up => 'new_user'}
 
   namespace :admin do
     resources :settings
     resources :dashboard
+    # resources :users_admin, :controller => 'users'
     resources :users
   end
 end
