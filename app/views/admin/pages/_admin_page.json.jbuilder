ca76d948bca3de8a9a2783fb4192aeda35665724
Pavel <pawelztef@gmail.com>
2019-07-03 15:50:57 +0100

    PAGES Pages resources added. Page model stays outstide admin scope, like rest of the models.

---
+++ app/views/admin/pages/_admin_page.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! admin_page, :id, :permalink, :content, :created_at, :updated_at
+json.url admin_page_url(admin_page, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :pages
+  end
   root to: 'welcome#index'
 
   # scope '/admin' do
