49580e6225f1c7f5d847bd1ea68ada4e946c21a7
Pavel <pawelztef@gmail.com>
2019-07-20 14:28:04 +0100

    Files strucuture for default front template created.

--- app/javascript/styles/index.js
+++ app/javascript/styles/index.js
@@ -3,4 +3,11 @@ import './auth.scss';
 import './dashboard.scss';
 import './admin_users.scss';
 import './preview.scss';
+import './front/content.scss';
+import './front/featured.scss';
+import './front/footer.scss';
+import './front/header.scss';
+import './front/hero.scss';
+import './front/navigation.scss';
+import './front/offcanvas.scss';
 
