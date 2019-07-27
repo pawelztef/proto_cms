0c92a350a7e6e91d3d4239421dc47f20d4e95d76
Pavel <pawelztef@gmail.com>
2019-07-27 09:25:32 +0100

    Destroy media action handle ajax calls.

--- app/controllers/admin/media_controller.rb
+++ app/controllers/admin/media_controller.rb
@@ -1,10 +1,14 @@
 class Admin::MediaController < Admin::AdminsController
   before_action :set_media, only: [:destroy]
+  before_action :all_media, only: [:destroy, :index]
+
   before_action :set_title
   skip_before_action :verify_authenticity_token
 
+  respond_to :html, :js
+
   def index
-    @all_media = Media.all
+    binding.pry
   end
 
   def new
@@ -12,6 +16,7 @@ class Admin::MediaController < Admin::AdminsController
   end
 
   def create
+    # TODO use sanitaize params
     respond_to do |format|
       @media = Media.new
       @media.attachment = params[:files][0]
@@ -24,15 +29,20 @@ class Admin::MediaController < Admin::AdminsController
   end
 
   def destroy
+    # binding.pry
     @media.destroy
     respond_to do |format|
-      format.html { redirect_to admin_media_index_path, notice: 'Attachement was successfully destroyed.' }
+      # format.html { redirect_to admin_media_index_path, notice: 'Attachement was successfully destroyed.' }
       format.js 
-      format.json { head :no_content }
+      # format.json { head :no_content }
     end
   end
 
   private
+
+  def all_media
+    @all_media = Media.all
+  end
 
   def set_media
     @media = Media.find(params[:id])
