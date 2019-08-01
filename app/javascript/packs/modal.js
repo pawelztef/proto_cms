2f7afe4f648fa8e4244fc6a43de69d2abb20890c
Pavel <pawelztef@gmail.com>
2019-08-01 10:47:06 +0100

    Modal javascript refactored to more generic function.

--- app/javascript/packs/modal.js
+++ app/javascript/packs/modal.js
@@ -1,76 +1,16 @@
-// const modal = document.getElementById('attachment-modal');
-// const modalCloseBtn = document.getElementById('close-modal-btn');
-// const modalContainer = document.getElementById('pz-modal-container'); 
-
-// if(modalContainer != null) {
-//   modalContainer.addEventListener('change', function() {
-//     if(modalCloseBtn != null) {
-//       modalCloseBtn.addEventListener('click', closeModal);
-//     }
-//   });
-// }
-
-
-// window.addEventListener('click', clickOutside);
-
-// function closeModal() {
-//   modal.style.display = 'none';
-// }
-// function clickOutside(e) {
-//   if(e.target == modal) {
-//     modal.style.display = 'none';
-//   }
-// }
-
 $(() => {
   window.Pz = window.Pz || {};
-  Pz.modal = window.Pz.modal || {};
 
-
-
-  Pz.modal.close =  function(element) {
-    const modal = document.getElementById('attachment-modal');
-    const modalCloseBtn = document.getElementById('close-modal-btn');
-    const modalContainer = document.getElementById('pz-modal-container'); 
-    console.log(element);
-    return this;
-  }
-
-  Pz.modal.init = function(element) {
-    if(element != null) {
-      openModal(element);
-      modalContainer.addEventListener('change', function() {
-        if(modalCloseBtn != null) {
-          modalCloseBtn.addEventListener('click', closeModal);
-        }
+  Pz.modal = function(modal, modalCloseBtn) {
+    if(modalCloseBtn != null) {
+      modalCloseBtn.addEventListener('click', function() {
+        modal.style.display = 'none';
       });
     }
-
-  }
-
-  // if(modalContainer != null) {
-  //   modalContainer.addEventListener('change', function() {
-  //     if(modalCloseBtn != null) {
-  //       modalCloseBtn.addEventListener('click', closeModal);
-  //     }
-  //   });
-  // }
-
-
-  // window.addEventListener('click', clickOutside);
-
-  function closeModalByButton(modal) {
-    modal.style.display = 'none';
-  }
-
-  function openModal(modal) {
-    modal.style.display = 'block';
-  }
-
-  function closeModalByClickOutside(e) {
-    if(e.target == modal) {
-      modal.style.display = 'none';
-    }
-  }
-
+    window.addEventListener('click', function(e) {
+      if(e.target == modal) {
+        modal.style.display = 'none';
+      }
+    });
+  };
 });
