3230c1450432255c63e1e92c791ba44e944202ba
Pavel <pawelztef@gmail.com>
2019-08-17 14:20:27 +0100

    Permalink, status columns have been added to posts table.

--- db/migrate/20190816123139_create_posts.rb
+++ db/migrate/20190816123139_create_posts.rb
@@ -2,10 +2,13 @@ class CreatePosts< ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
       t.string :title
+      t.string :permalink
       t.text :content
       t.string :summary
+      t.integer :status, default: 0
 
       t.timestamps
     end
+    add_index :posts, :permalink
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -54,10 +54,13 @@ ActiveRecord::Schema.define(version: 2019_08_16_123139) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
+    t.string "permalink"
     t.text "content"
     t.string "summary"
+    t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["permalink"], name: "index_posts_on_permalink"
   end
 
   create_table "settings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
