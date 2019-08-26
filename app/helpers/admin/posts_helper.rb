94e077294c107cb2eab9305914034180865e114b
Pavel <pawelztef@gmail.com>
2019-08-26 20:38:31 +0100

    Right column in posts index revmoed.

--- app/helpers/admin/posts_helper.rb
+++ app/helpers/admin/posts_helper.rb
@@ -1,2 +1,7 @@
 module Admin::PostsHelper
+  def status_label(status)
+    tag.span class: status == "published" ? "uk-label uk-label-default" : "uk-label uk-label-warning" do
+      status
+    end
+  end
 end
