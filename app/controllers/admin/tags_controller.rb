92bc7786e4cf12969cb3f5276bb429dccaeed653
Pavel <pawelztef@gmail.com>
2019-08-27 21:18:34 +0100

    Tag model moved from admin namespce.

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -1,30 +1,22 @@
 class Admin::TagsController < ApplicationController
   before_action :set_admin_tag, only: [:show, :edit, :update, :destroy]
 
-  # GET /admin/tags
-  # GET /admin/tags.json
   def index
-    @admin_tags = Admin::Tag.all
+    @admin_tags = Tag.all
   end
 
-  # GET /admin/tags/1
-  # GET /admin/tags/1.json
   def show
   end
 
-  # GET /admin/tags/new
   def new
-    @admin_tag = Admin::Tag.new
+    @admin_tag = Tag.new
   end
 
-  # GET /admin/tags/1/edit
   def edit
   end
 
-  # POST /admin/tags
-  # POST /admin/tags.json
   def create
-    @admin_tag = Admin::Tag.new(admin_tag_params)
+    @admin_tag = Tag.new(admin_tag_params)
 
     respond_to do |format|
       if @admin_tag.save
@@ -37,8 +29,6 @@ class Admin::TagsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /admin/tags/1
-  # PATCH/PUT /admin/tags/1.json
   def update
     respond_to do |format|
       if @admin_tag.update(admin_tag_params)
@@ -51,8 +41,6 @@ class Admin::TagsController < ApplicationController
     end
   end
 
-  # DELETE /admin/tags/1
-  # DELETE /admin/tags/1.json
   def destroy
     @admin_tag.destroy
     respond_to do |format|
@@ -62,12 +50,10 @@ class Admin::TagsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_admin_tag
-      @admin_tag = Admin::Tag.find(params[:id])
+      @admin_tag = Tag.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def admin_tag_params
       params.require(:admin_tag).permit(:name)
     end
