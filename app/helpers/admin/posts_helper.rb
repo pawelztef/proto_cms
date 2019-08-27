67f63df1ded0b1aa3ebd2d0a83e21f35caa3a4f5
Pavel <pawelztef@gmail.com>
2019-08-27 18:31:36 +0100

    Posts helper created. Ceates in-table category labels.

--- app/helpers/admin/posts_helper.rb
+++ app/helpers/admin/posts_helper.rb
@@ -1,7 +1,17 @@
 module Admin::PostsHelper
-  def status_label(status)
-    tag.span class: status == "published" ? "uk-label uk-label-default" : "uk-label uk-label-warning" do
-      status
+  def generate_status_label post
+    tag.span class: post.status == "published" ? "uk-label uk-label-default" : "uk-label uk-label-warning" do
+      post.status
     end
   end
+
+
+  def generate_categories_labels post
+    tags = ""
+    post.categories.each do |cat|
+      tags << content_tag(:span, cat.name, class: "uk-label uk-label-default admin__posts-index_category-label").html_safe
+    end
+    tags.html_safe
+  end
+
 end
