0b2917e1921c9c70b904ad71410d81b2ce187981
Pavel <pawelztef@gmail.com>
2019-08-29 20:49:12 +0100

    Slugify method implemented in tags

--- app/javascript/packs/categories.js
+++ app/javascript/packs/categories.js
@@ -1,10 +1,10 @@
 const multiSelect = function() {
   const categoriesSelector = document.querySelector("#new_post_categories");
   const postAssociationField = document.querySelector("#post_category_ids");
-  const slimSelect = new SlimSelect({
-    select: '#new_post_categories'
-  });
   if(categoriesSelector != null && postAssociationField != null) {
+    const slimSelect = new SlimSelect({
+      select: '#new_post_categories'
+    });
     categoriesSelector.addEventListener('change', function(e) {
       e.preventDefault();
       e.stopPropagation();
