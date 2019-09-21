770d3d8ffd09bab23e28b1df1e3991caa198669e
Pavel <pawelztef@gmail.com>
2019-09-21 18:32:18 +0100

    Created js file for trix conf

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,6 +27,7 @@ require('packs/modal.js');
 require('packs/slugify.js');
 require('packs/images_upload.js');
 require('packs/image_edit.js.erb');
+require('packs/trix_editor.js.erb');
 
 require.context('../images', true);
 
