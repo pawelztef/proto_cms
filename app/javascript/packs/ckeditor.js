58802988d9c4f2341121fe4a85efca1331196203
Pavel <pawelztef@gmail.com>
2019-10-24 13:47:35 +0100

    Ckeditor added to pages.

--- app/javascript/packs/ckeditor.js
+++ app/javascript/packs/ckeditor.js
@@ -18,6 +18,24 @@ $(() => {
         ];
         config.removeButtons = 'Print,Preview,NewPage,Save,Templates,Cut,Copy,Paste,PasteText,PasteFromWord,Find,Replace,Scayt,SelectAll,Form,Checkbox,Radio,TextField,Textarea,Button,Select,ImageButton,HiddenField,Subscript,Superscript,RemoveFormat,CopyFormatting,BidiRtl,BidiLtr,Language,Unlink,Anchor,Image,Flash,HorizontalRule,Smiley,SpecialChar,PageBreak,Iframe,Table,Styles,About,Outdent,Indent';
       };
+  }
 
+  if(document.getElementById('page_content'))  {
+      CKEDITOR.replace( 'page_content', {
+        customConfig: 'ckeditor.js',
+        allowedContent: true
+      });
+      CKEDITOR.editorConfig = function( config ) {
+        config.toolbarGroups = [
+          { name: 'styles', groups: [ 'styles' ] },
+          { name: 'basicstyles', groups: [ 'basicstyles', 'align' ] },
+          '/',
+          { name: 'paragraph', groups: [  'list', 'links'] },
+          { name: 'document', groups: [ 'blocks', 'blocks', 'mode'] },
+          { name: 'tools', groups: [ 'tools' ] },
+          { name: 'clipboard', groups: [ 'undo' ] }
+        ];
+        config.removeButtons = 'Print,Preview,NewPage,Save,Templates,Cut,Copy,Paste,PasteText,PasteFromWord,Find,Replace,Scayt,SelectAll,Form,Checkbox,Radio,TextField,Textarea,Button,Select,ImageButton,HiddenField,Subscript,Superscript,RemoveFormat,CopyFormatting,BidiRtl,BidiLtr,Language,Unlink,Anchor,Image,Flash,HorizontalRule,Smiley,SpecialChar,PageBreak,Iframe,Table,Styles,About,Outdent,Indent';
+      };
   }
 })
