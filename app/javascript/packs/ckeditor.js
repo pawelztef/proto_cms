9b5477a0962e0856c24d471d7828d068d88c5d0b
Pavel <pawelztef@gmail.com>
2019-10-24 14:01:09 +0100

    Update to ckeditor config file.

--- app/javascript/packs/ckeditor.js
+++ app/javascript/packs/ckeditor.js
@@ -1,41 +1,26 @@
 $(() => { 
+
+  const ckSettings = {
+    customConfig: 'ckeditor.js',
+    allowedContent: true,
+    removeButtons: 'Print,Preview,NewPage,Save,Templates,Cut,Copy,Paste,PasteText,PasteFromWord,Find,Replace,Scayt,SelectAll,Form,Checkbox,Radio,TextField,Textarea,Button,Select,ImageButton,HiddenField,Subscript,Superscript,RemoveFormat,CopyFormatting,BidiRtl,BidiLtr,Language,Unlink,Anchor,Image,Flash,HorizontalRule,Smiley,SpecialChar,PageBreak,Iframe,Table,Styles,About,Outdent,Indent',
+    toolbarGroups: [
+      { name: 'styles', groups: [ 'styles' ] },
+      { name: 'basicstyles', groups: [ 'basicstyles', 'align' ] },
+      '/',
+      { name: 'paragraph', groups: [  'list', 'links'] },
+      { name: 'document', groups: [ 'blocks', 'blocks', 'mode'] },
+      { name: 'tools', groups: [ 'tools' ] },
+      { name: 'clipboard', groups: [ 'undo' ] }
+    ]
+  }
+
   if(document.getElementById('post_content'))  {
-
-      CKEDITOR.replace( 'post_content', {
-        customConfig: 'ckeditor.js',
-        allowedContent: true
-      });
-
-      CKEDITOR.editorConfig = function( config ) {
-        config.toolbarGroups = [
-          { name: 'styles', groups: [ 'styles' ] },
-          { name: 'basicstyles', groups: [ 'basicstyles', 'align' ] },
-          '/',
-          { name: 'paragraph', groups: [  'list', 'links'] },
-          { name: 'document', groups: [ 'blocks', 'blocks', 'mode'] },
-          { name: 'tools', groups: [ 'tools' ] },
-          { name: 'clipboard', groups: [ 'undo' ] }
-        ];
-        config.removeButtons = 'Print,Preview,NewPage,Save,Templates,Cut,Copy,Paste,PasteText,PasteFromWord,Find,Replace,Scayt,SelectAll,Form,Checkbox,Radio,TextField,Textarea,Button,Select,ImageButton,HiddenField,Subscript,Superscript,RemoveFormat,CopyFormatting,BidiRtl,BidiLtr,Language,Unlink,Anchor,Image,Flash,HorizontalRule,Smiley,SpecialChar,PageBreak,Iframe,Table,Styles,About,Outdent,Indent';
-      };
+    CKEDITOR.replace( 'post_content', ckSettings );
   }
 
   if(document.getElementById('page_content'))  {
-      CKEDITOR.replace( 'page_content', {
-        customConfig: 'ckeditor.js',
-        allowedContent: true
-      });
-      CKEDITOR.editorConfig = function( config ) {
-        config.toolbarGroups = [
-          { name: 'styles', groups: [ 'styles' ] },
-          { name: 'basicstyles', groups: [ 'basicstyles', 'align' ] },
-          '/',
-          { name: 'paragraph', groups: [  'list', 'links'] },
-          { name: 'document', groups: [ 'blocks', 'blocks', 'mode'] },
-          { name: 'tools', groups: [ 'tools' ] },
-          { name: 'clipboard', groups: [ 'undo' ] }
-        ];
-        config.removeButtons = 'Print,Preview,NewPage,Save,Templates,Cut,Copy,Paste,PasteText,PasteFromWord,Find,Replace,Scayt,SelectAll,Form,Checkbox,Radio,TextField,Textarea,Button,Select,ImageButton,HiddenField,Subscript,Superscript,RemoveFormat,CopyFormatting,BidiRtl,BidiLtr,Language,Unlink,Anchor,Image,Flash,HorizontalRule,Smiley,SpecialChar,PageBreak,Iframe,Table,Styles,About,Outdent,Indent';
-      };
+    CKEDITOR.replace( 'page_content', ckSettings );
   }
+
 })
