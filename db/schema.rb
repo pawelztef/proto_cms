c3d3f81d88966fa1864dca904270b8edf8282cad
Pavel <pawelztef@gmail.com>
2019-10-28 10:35:45 +0000

    Adding STI migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_28_073653) do
+ActiveRecord::Schema.define(version: 2019_10_28_103534) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 2019_10_28_073653) do
     t.datetime "updated_at", null: false
     t.integer "status", default: 0
     t.string "ancestry"
+    t.string "type"
     t.index ["ancestry"], name: "index_pages_on_ancestry"
     t.index ["permalink"], name: "index_pages_on_permalink"
     t.index ["status"], name: "index_pages_on_status"
