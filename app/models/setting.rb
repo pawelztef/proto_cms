04ae64dbc4b4ee9e7e2547b174f19d4cf1e53e65
Pavel <pawelztef@gmail.com>
2019-11-02 17:21:44 +0000

    Virtual attribute in setting.

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -2,9 +2,11 @@ class Setting < ApplicationRecord
   # The "singleton_guard" column is a unique column which must always be set to '0'
   # This ensures that only one AppSettings row is created
   validates_inclusion_of :singleton_guard, :in => [0]
+  attr_accessor :home_page
 
   def self.instance
     first_or_create!(singleton_guard: 0)
   end
+
 
 end
