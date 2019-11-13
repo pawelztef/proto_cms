ae4ce1db502013725191a316cede8e9b3927ec55
Pavel <pawelztef@gmail.com>
2019-11-13 08:39:16 +0000

    Favicon helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,5 +7,9 @@ module ApplicationHelper
     return "#{(post.content.split.size/2.16/60).ceil} min read"
   end
 
+  def site_favicon_tag
+    favicon_link_tag url_for(Site.instance.favicon) if Site.instance.favicon.attached?
+  end
+
 
 end
