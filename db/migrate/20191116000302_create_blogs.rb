336347d904cd81930cb4213e42beb933fe25ee54
Pavel <pawelztef@gmail.com>
2019-11-18 10:05:35 +0000

    Blogs table update. Max comments nesting default value has beens set to 1.

--- db/migrate/20191116000302_create_blogs.rb
+++ db/migrate/20191116000302_create_blogs.rb
@@ -5,7 +5,7 @@ class CreateBlogs < ActiveRecord::Migration[5.2]
       t.boolean :visible, default: false
       t.string :permalink
       t.boolean :commentable, default: false
-      t.integer :max_comment_nesting
+      t.integer :max_comment_nesting, default: 1
       t.references :site, foreign_key: true
 
       t.timestamps
