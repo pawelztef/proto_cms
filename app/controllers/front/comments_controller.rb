d3b1cb5d5da82d736de9f5f0d59c05f1d9806822
Pavel <pawelztef@gmail.com>
2019-11-19 22:36:28 +0000

    Removing ajax in commentary.

--- app/controllers/front/comments_controller.rb
+++ app/controllers/front/comments_controller.rb
@@ -1,28 +1,25 @@
 class Front::CommentsController < ApplicationController
-    layout "front"
-    before_action :authenticate_reader!
-    
-    def new
-      @comment = Comment.new
-    end
+  layout "front"
+  before_action :authenticate_reader!
 
-    def create
-      @comment = @commentable.comments.new(comment_params)
-      @comment.reader = current_reader
-      if @comment.save
-        respond_to do |format|
-          format.html { redirect_to @commentable }
-          format.js
-        end
-      else
-        redirect_to @commentable, alert: "Somethings went wrong"
-      end
-    end
+  def new
+    @comment = Comment.new
+  end
 
-    private
-
-    def comment_params
-      params.require(:comment).permit(:body, :parent_id)
+  def create
+    @comment = @commentable.comments.new(comment_params)
+    @comment.reader = current_reader
+    if @comment.save
+      redirect_to @commentable 
+    else
+      redirect_to @commentable, alert: "Somethings went wrong"
     end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :parent_id)
+  end
 
 end
