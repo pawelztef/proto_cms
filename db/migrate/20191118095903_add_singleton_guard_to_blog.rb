89edb606ffbb62fa9882ba6fb484fe35ddf4f4b1
Pavel <pawelztef@gmail.com>
2019-11-29 14:07:54 +0000

    Singelton guard removed from Blog.

--- db/migrate/20191118095903_add_singleton_guard_to_blog.rb
+++
@@ -1,8 +0,0 @@
-class AddSingletonGuardToBlog < ActiveRecord::Migration[5.2]
-  def up 
-    add_column :blogs, :singleton_guard, :integer, default: 0, unique: true
-  end
-  def down
-    remove_column :blogs, :singleton_guard
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_18_095903) do
+ActiveRecord::Schema.define(version: 2019_11_17_134524) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -42,7 +42,6 @@ ActiveRecord::Schema.define(version: 2019_11_18_095903) do
     t.bigint "site_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "singleton_guard", default: 0
     t.index ["site_id"], name: "index_blogs_on_site_id"
   end
 
