ebca7d7a48e7cc7accc53842d217bb8ad884e547
Pavel <pawelztef@gmail.com>
2019-12-01 14:24:27 +0000

    Pages migration refactored to fulfill functions of Blogs and Posts. As a fundament cms strucutre Page model will have blog and post attributes.

--- db/migrate/20190703144726_create_pages.rb
+++ db/migrate/20190703144726_create_pages.rb
@@ -3,8 +3,14 @@ class CreatePages < ActiveRecord::Migration[5.2]
     create_table :pages do |t|
       t.string :name
       t.string :permalink
+      t.string :type
       t.text :content
+      t.text :summary
+      t.integer :status, default: 0
+      t.boolean :commentable, default: false
+      t.integer :max_comment_nesting, default: 1
 
+      t.references :site, foreign_key: true
       t.timestamps
     end
     add_index :pages, :permalink
