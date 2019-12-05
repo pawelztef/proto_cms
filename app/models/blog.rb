170724e5f0dc921e575e2d117c1dc21e82464e20
Pavel <pawelztef@gmail.com>
2019-12-05 08:55:32 +0000

    Alias added to Blog.

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -30,9 +30,7 @@
 
 class Blog < Page 
 
-  def posts
-    self.children
-  end
+  alias posts children
 
   def self.instance
     first_or_create!(site: Site.instance, title: "Blog", permalink: "blog", status: 1) 
