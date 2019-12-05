5f9358ebaefcf096673c77319d1e91f2b74403a3
Pavel <pawelztef@gmail.com>
2019-12-05 08:39:57 +0000

    Switch in show action.

--- app/controllers/front/pages_controller.rb
+++ app/controllers/front/pages_controller.rb
@@ -1,17 +1,40 @@
 class Front::PagesController < ApplicationController
-  layout 'front/themes/layouts/front'
+  layout :set_layout
+
+  before_action :set_page
+  append_before_action :check_visibility, only: :show, unless: -> { @page.nil? }
+
   def index
     @page = HomePage.instance
     render 'front/themes/pages/show'
   end
 
   def show
-    @page = Page.find_by_permalink(params[:permalink]) 
-    if @page.visible
+    case @page.class.name
+    when "HomePage"
       render 'front/themes/pages/show'
-    else 
-      # TODO redirect to 404
-      redirect_to root_path
+    when "Blog"
+      render 'front/themes/posts/index'
+    else
+      render 'front/themes/pages/show'
+    end
+  end
+
+  private 
+
+  def set_page
+    @page = Page.find_by_permalink(params[:permalink])
+  end
+
+  def check_visibility
+    redirect_to root_path unless @page.visible?
+  end
+
+  def set_layout
+    if @page.is_a?(Blog)
+      'front/themes/layouts/blog'
+    else
+      'front/themes/layouts/front'
     end
   end
 
