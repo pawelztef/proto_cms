443183be417cc4fc668bd6aeda7beb241fc89916
Pavel <pawelztef@gmail.com>
2019-12-11 21:28:46 +0000

    Cleaning publishable_controller.

--- app/controllers/front/publishables_controller.rb
+++ app/controllers/front/publishables_controller.rb
@@ -1,36 +1,10 @@
 class Front::PublishablesController < ApplicationController
-  layout :set_layout
 
-  before_action :set_publishable
-  append_before_action :check_status , only: :show, unless: -> { @publishable.nil? }
 
   def index
-    @publishable = Site.instance.home_page
-    render 'front/themes/pages/show'
-  end
-
-  def show
-    case @publishable.class.name
-    when "HomePage"
-      redirect_to root_url
-    when "Blog"
-      @posts = Blog.instance.posts
-      render 'front/themes/posts/index'
-    when "Post"
-      @publishable.permalink  = [@publishable.parent.permalink, @publishable.permalink].join('/') if @publishable.parent.present?
-      byebug
-      render 'front/themes/posts/show'
-    else
-      render 'front/themes/pages/show'
-    end
-  end
-
-  def blog
-
-  end
-
-  def new_comment
     byebug
+    @publishable = Site.instance.home_page
+    render 'front/themes/pages/show', layout: default_layout
   end
 
   private 
@@ -43,12 +17,12 @@ class Front::PublishablesController < ApplicationController
     redirect_to root_path unless @publishable.published?
   end
 
-  def set_layout
-    if @publishable.is_a?(Blog)
-      'front/themes/layouts/blog'
-    else
-      'front/themes/layouts/front'
-    end
+  def blog_layout 
+    'front/themes/layouts/blog'
+  end
+
+  def default_layout
+    'front/themes/layouts/pages'
   end
 
 end
