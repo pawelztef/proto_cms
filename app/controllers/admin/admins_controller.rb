a8b9088a7c5f48148a3e4d746caee2b733a63cd4
Pavel <pawelztef@gmail.com>
2020-01-04 18:43:52 +0000

    Updates to admin controller & main admin views.

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -1,5 +1,10 @@
 class Admin::AdminsController < ApplicationController
   before_action :authenticate_user!
+  before_action :publishable_groups
   layout 'admin/layouts/admin'
   skip_before_action :verify_authenticity_token
+
+  def publishable_groups
+    @publishable_groups =  PublishableGroup.all
+  end
 end
