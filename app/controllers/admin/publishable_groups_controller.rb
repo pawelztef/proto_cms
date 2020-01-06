e41e3cd5879d3253712e043adad0944b442e7326
Pavel <pawelztef@gmail.com>
2020-01-06 08:09:11 +0000

    Find publishables in publishable group controller.

--- app/controllers/admin/publishable_groups_controller.rb
+++ app/controllers/admin/publishable_groups_controller.rb
@@ -1,11 +1,11 @@
 class Admin::PublishableGroupsController < Admin::AdminsController
 
   def index
-byebug
+    byebug
   end
 
   def show
-byebug
+    @publishables = PublishableGroup.find_by_permalink(params[:publishable_group_id]).publishables
   end
 
 end
