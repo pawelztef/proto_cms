2f5e63aeccaaa1d68e389bbe7b8e3cbe5433e646
Pavel <pawelztef@gmail.com>
2020-02-21 09:44:50 +0000

    Taxonomy handle tags.

--- app/models/taxonomy.rb
+++ app/models/taxonomy.rb
@@ -13,4 +13,5 @@ class Taxonomy < ApplicationRecord
   belongs_to :publishable
   belongs_to :taxon
   belongs_to :category, class_name: "Taxon", foreign_key: "taxon_id"
+  belongs_to :tag, class_name: "Taxon", foreign_key: "taxon_id"
 end
